f36be212506df813ae45e60d3b9702366033a8e5
PrettyLolli <yunghunters2015@gmail.com>
2024-03-05 01:58:37 +0300

    final

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import { connect } from "@vue/devtools-electron";
 import Vue from 'vue'
 import App from './App.vue'
 
-if (process.env.NODE_ENV === 'development') {
-  connect('http://localhost', 8098)
-}
+
 Vue.config.productionTip = false;
+
+Vue.config.devtools = true;
+
+//console.log(Vue.config.devtools);
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
